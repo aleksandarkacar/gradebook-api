a36a539a27f445bb97d12ae54f5f812ccd2f0a0d
Ammo95 <kacar95@gmail.com>
2023-04-06 19:50:25 +0200

    Napravljene migracije i modeli, postavljeni prazni controlleri

--- database/migrations/2023_04_06_172138_create_students_table.php
+++ database/migrations/2023_04_06_172138_create_students_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('gradebook_id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('img_url');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
