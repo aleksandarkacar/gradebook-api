f1b1a1cc5507fc0c8ddb2cea0c834bd9c9513e87
Ammo95 <kacar95@gmail.com>
2023-04-06 20:04:57 +0200

    relacije namestene

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'gradebook_id', 'body'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function gradebook(): BelongsTo
+    {
+        return $this->belongsTo(Gradebook::class);
+    }
 }
--- app/Models/Gradebook.php
+++ app/Models/Gradebook.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Gradebook extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'name'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
 }
